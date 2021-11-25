43a6eed1e6b42f39a643c4549e931d2e8d1e08ad
xcsoft <email@xcsoft.top>
2021-11-25 10:59:17 +0000

    safeLoad

--- autoshield_main.py
+++ autoshield_main.py
@@ -10,13 +10,16 @@ import public
 import os
 import json
 import requests
+import psutil
 
 PLUGIN_NAME = 'autoshield'
 FIREWALL_SERVICE_NAME = 'autoshield.py'
 PLUGIN_PATH = "/www/server/panel/plugin/{}/".format(PLUGIN_NAME)
 
-SETTING_FILE_PATH = PLUGIN_PATH + 'config/setting.json'  # 设置temp文件路径
+SETTING_FILE_PATH = PLUGIN_PATH + 'config/setting.json'  # setting文件路径
+SAFE_FILE_PATH = PLUGIN_PATH + 'config/safe.json'  # safe文件路径
 DOMAIN_FILE_PATH = PLUGIN_PATH + 'config/domain.json'  # 用户域名temp文件路径
+DOMAIN_DNS_BASE_PATH = PLUGIN_PATH + 'config/dns/'  # 用户域名temp文件路径
 
 PER_PAGE = 500  # 获取的域名个数 值应该在1到1000之间
 
@@ -40,8 +43,12 @@ class autoshield_main:
 
     # 获取cloudflare key & email
     def get_setting(self, args):
+        default = {
+            'email': "",
+            'cfkey': "",
+        }
         if not os.path.exists(SETTING_FILE_PATH):
-            return {'key': '', 'email': ''}
+            public.WriteFile(SETTING_FILE_PATH, json.dumps(default), mode='w+')
         try:
             data = json.loads(public.ReadFile(SETTING_FILE_PATH, mode='r'))
             return {
@@ -49,25 +56,8 @@ class autoshield_main:
                 'email': data['email'] if data['email'] else '',
             }
         except:
-            public.WriteFile(SETTING_FILE_PATH, json.dumps({
-                'email': "",
-                'cfkey': "",
-            }), mode='w+')
-
-        return {'key': '', 'email': ''}
-
-    # 设置cloudflare key & email
-    def do_setting(self, args):
-        email = args['email']
-        key = args['key']
-        if not email or not key:
-            return {'status': False, 'msg': '<h3>必填项不能为空</h3>'}
-
-        public.WriteFile(SETTING_FILE_PATH, json.dumps({
-            'email': email,
-            'key': key
-        }), mode='w+')
-        return {'status': True, 'msg': 'success'}
+            public.WriteFile(SETTING_FILE_PATH, json.dumps(default), mode='w+')
+        return default
 
     def get_domain(self, args):
         try:
@@ -77,25 +67,104 @@ class autoshield_main:
         except:
             return {'status'}
 
+    # 获取防御等级
+    def get_safe(self, args):
+        default = {
+            "wait": "300",  # 负载恢复后的等待周期
+            "sleep": "5",  # 检测周期
+            "check": "30",  # 持续监测时间
+            # "back": 'medium',  # 负载恢复后 状态切换
+            "load": '10'      # 负载阀值
+        }
+        if not os.path.exists(SAFE_FILE_PATH):
+            public.WriteFile(SAFE_FILE_PATH, json.dumps(default), mode='w+')
+        try:
+            data = json.loads(public.ReadFile(SAFE_FILE_PATH, mode='r'))
+            return {
+                "wait": data['wait'] if data['wait'] else '300',
+                "sleep": data['sleep'] if data['sleep'] else '5',
+                "check": data['check'] if data['check'] else '30',
+                # "back": data['back'] if data['back'] else 'medium',
+                "load": data['load'] if data['load'] else '10',
+            }
+        except:
+            public.WriteFile(SAFE_FILE_PATH, json.dumps(default), mode='w+')
+        return default
+
+    # 获取安全负载
+    def get_safe_load(self, args):
+        cpuCount = psutil.cpu_count()
+        safe_load = cpuCount * 1.75
+        return {'cpu_count': cpuCount, 'safe_load': safe_load}
+
+    # 设置cloudflare key & email
+
+    def set_setting(self, args):
+        email = args['email']
+        key = args['key']
+        if not email or not key:
+            return {'code': -1, 'msg': '必填项不能为空'}
+
+        public.WriteFile(SETTING_FILE_PATH, json.dumps({
+            'email': email,
+            'key': key
+        }), mode='w+')
+        return {'msg': 'success'}
+
+    # 刷新域名列表
     def refresh_domain(self, args):
-        cf = Cloudflare()
-        response = cf.getDomain()
+        response = Cloudflare().getDomain()
         if response['success']:
             # 获取成功
             count = response['result_info']['count']  # 域名数量
             result = response['result']  # 域名信息
 
             data = {}  # 初始化data
+            index = []  # 域名索引
             for v in result:
-                data[v['name']] = v['id']
+                data[v['name']] = {
+                    'id': v['id'],
+                    'security': "unknow",
+                    'status': True
+                }
+                public.WriteFile(
+                    DOMAIN_DNS_BASE_PATH + v['name'] + '.json',
+                    "{}",
+                    mode='w+'
+                )
+                index.append(v['name'])
             res = {
                 'count': count,
                 'domains': data,
+                'index': index
             }
             public.WriteFile(DOMAIN_FILE_PATH, json.dumps(res), mode='w+')
-            return {'code': 200, 'msg': 'success'}
+            public.WriteLog(PLUGIN_NAME, '刷新域名列表成功')
+            return {'code': 200, 'msg': 'success', 'count': count}
         # 获取失败
-        return {'status': False, 'msg': "<h3>邮箱或API密钥错误</h3> <br/>返回信息:<br/>" + json.dumps(response['errors'])}
+        public.WriteLog(
+            PLUGIN_NAME,
+            "尝试登录时遇到错误 > " + json.dumps(response['errors'])
+        )
+        return {'code': -1, 'msg': "邮箱或API密钥错误<br/>(您可以在面板安全板块查询详细错误信息)"}
+
+    def refresh_domain_security(self, args):
+        domainList = json.loads(public.ReadFile(DOMAIN_FILE_PATH, mode='r'))
+        for domainName, v in domainList['domains'].items():
+            domainId = v['id']  # 域名信息
+            domainInfo = Cloudflare().getSecurity(domainId)
+            if domainInfo['success']:  # 获取成功
+                domainList['domains'][domainName]['security'] = domainInfo['result']['value']
+            else:
+                public.WriteLog(
+                    PLUGIN_NAME,
+                    '获取域名{}防御等级时出现错误 > {}'.format(
+                        domainName, json.dumps(domainInfo['errors']))
+                )
+        public.WriteFile(DOMAIN_FILE_PATH, json.dumps(domainList), mode='w+')
+        return {
+            'code': 200,
+        }
 
 
 class Cloudflare:
@@ -111,6 +180,14 @@ class Cloudflare:
         response = self.__get('zones', {
             'per_page': PER_PAGE  # 拉满
         })
+        return response
+
+    # 获取域名防御等级
+    def getSecurity(self, domainId):
+        response = self.__get(
+            'zones/{}/settings/security_level'.format(domainId),
+            {}
+        )
         return response
 
     def __post(self, url, data):
